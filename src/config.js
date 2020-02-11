7e093cadd262be46d356f34f8a53c605f5189fec
RMayeux <remimayeux@gmail.com>
2020-02-11 15:03:49 +0100

    Setting up our react app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+import Amplify from 'aws-amplify';
+
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-uploads-rm"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://9qgml5i17e.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_0DJdJ1ixm",
+      APP_CLIENT_ID: "2d8amqdh1dsfjo5mr6unpbtaur",
+      IDENTITY_POOL_ID: "us-east-2:cdde75cc-33d8-47f8-8204-9bdf5df0f18b"
+    }
+  };
